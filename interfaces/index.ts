d692ccd470c4772122146e82c0b95f3454b9c2fa
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-08-02 00:11:56 +0300

    finished milestone 2

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -14,3 +14,24 @@ export interface ButtonProps {
   className?: string; // Optional: extra CSS classes for styling
   disabled?: boolean; // Optional: disables the button if true
 }
+
+export interface Address {
+  state: string;
+  city: string;
+  country: string;
+}
+export interface Offers {
+  bed: string;
+  shower: string;
+  occupants: string;
+}
+export interface PropertyProps {
+  name: string;
+  address: Address;
+  rating: number;
+  category: string[];
+  price: number;
+  offers: Offers;
+  image: string;
+  discount: string;
+}
